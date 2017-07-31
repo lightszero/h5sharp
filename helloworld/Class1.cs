eeea48f1890b4d2408f54bed359be04c788e00b5
lights li <lightsever@hotmail.com>
2017-07-31 23:29:02 +0800

    blazor 研究報告

---
+++ helloworld/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("run a dll on web.");
+
+
+        }
+    }
+}
